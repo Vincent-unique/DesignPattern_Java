993ebac9f8e39ab712c29c4b89d3ac8065401d44
vincent <vincent_unique@163.com>
2018-01-26 17:03:41 +0800

    add test for thread Runtime breaker

---
+++ src/test/java/org/trump/vincent/ThreadBreaker.java
@@ -0,0 +1,43 @@
+package org.trump.vincent;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Vincent on 2018/1/26 0026.
+ */
+public class ThreadBreaker {
+
+    protected static class Handler implements Runnable{
+        public void run() {
+            AtomicInteger counter = new AtomicInteger(0);
+            while (true){
+                counter.getAndIncrement();
+                if(counter.getAndIncrement()>10){
+                    throw new RuntimeException("Thread Runtime Breaker.");
+                }
+                System.out.println("Health.");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Handler(),"Handle");
+        thread.start();
+        try {
+            TimeUnit.SECONDS.sleep(8);
+            System.out.println("Handle is alive:\t" + thread.isAlive());
+            if(!thread.isAlive()){
+                thread = new Thread(new Handler(),"Handle");
+                thread.start();
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
